bbe650030256400fd0138e63fa4b75a0dbaae0b0
Manuel López <manuellp87@gmail.com>
2019-01-20 01:23:03 +0100

    Los botones se crean programaticamente

--- testProjectSound/ViewController.swift
+++ testProjectSound/ViewController.swift
@@ -16,9 +16,42 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Botones programaticamente
+        createButtons()
+        
     }
     
-    @IBAction func pressButton(_ sender: UIButton) {
+    func createButtons (){
+        let colors = [1:UIColor.red,
+                      2:UIColor.blue,
+                      3:UIColor.green,
+                      4:UIColor.yellow,
+                      5:UIColor.cyan,
+                      6:UIColor.brown,
+                      7:UIColor.orange]
+        
+        for i in 1...7 {
+            
+            let button = UIButton()
+            
+            button.translatesAutoresizingMaskIntoConstraints = false
+            
+            button.backgroundColor = .blue
+            
+            self.view.addSubview(button)
+            button.topAnchor.constraint(equalTo: self.view.topAnchor, constant: CGFloat(0 + 80 * i)).isActive = true
+            button.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -20).isActive = true
+            button.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20).isActive = true
+            button.heightAnchor.constraint(equalToConstant: 60).isActive = true
+            
+            button.backgroundColor = colors[i]
+            button.tag = i
+            button.addTarget(self, action: #selector(pressButton), for: .touchUpInside)
+        }
+    }
+    
+    @objc func pressButton(sender: UIButton!) {
 //        print(String(sender.tag))
         playSound(audioTag: String(sender.tag))
     }
@@ -39,10 +72,6 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         } catch let error{
             print(error.localizedDescription)
         }
-        
-        
     }
-
-
 }
 
